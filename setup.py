6f8c2e83a7b474be287febb16c9d49c61f258aef
iorp <104486888+iorp@users.noreply.github.com>
2023-11-19 00:35:29 -0800

    Fixed deprecation of wheel setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-# from setuptools import setup
-# from Cython.Build import cythonize
-
-# setup(
-#     name='mylibrary',
-#     ext_modules=cythonize(['mylibrary/mypackage1/*.pyx', 'mylibrary/mypackage2/*.pyx']),
-#     packages=['mylibrary', 'mylibrary.mypackage1', 'mylibrary.mypackage2'],
-#     package_dir={'mylibrary': 'mylibrary'},
-#     zip_safe=False,
-# )
-
+ 
 
 from setuptools import setup
 from Cython.Build import cythonize
